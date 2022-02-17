c23d0208b9bb5941f67b98d410809f3f3bd69aa7
harrdev <jasonharrphx@live.com>
2022-02-17 11:14:43 -0700

    added species edit route

--- app/routes/swspecies.js
+++ app/routes/swspecies.js
@@ -13,25 +13,46 @@ const Saved = require('../models/species')
 const router = express.Router()
 
 router.get('/FaveSpecies', requireToken, (req, res, next) => {
-	Saved.find()
-		.then((species) => {
-			const userSpecies = species.filter(species => species.owner.toString() === req.user.id)
-			return userSpecies.map((species) => species.toObject())
-		})
-		.then((species) => res.status(200).json({ species: species }))
-		.catch(next)
+    Saved.find()
+        .then((species) => {
+            const userSpecies = species.filter(species => species.owner.toString() === req.user.id)
+            return userSpecies.map((species) => species.toObject())
+        })
+        .then((species) => res.status(200).json({ species: species }))
+        .catch(next)
+})
+
+router.patch('/Species/:id', requireToken, (req, res, next) => {
+    Saved.findOneAndUpdate({
+        "_id": req.params.id
+    }, {
+        "$set": {
+            name: req.body.info.name,
+            classification: req.body.info.classification,
+            average_height: req.body.info.average_height,
+            skin_colors: req.body.info.skin_colors,
+            hair_colors: req.body.info.hair_colors,
+            eye_colors: req.body.info.eye_colors,
+            average_lifespan: req.body.info.average_lifespan,
+            language: req.body.info.language
+        }
+    })
+        // .then(handle404)
+        .then(() => res.sendStatus(204))
+        // if an error occurs, pass it to the handler
+        .catch(next)
 })
 
 router.delete('/FaveSpecies/:id', requireToken, (req, res, next) => {
-	Saved.findOneAndDelete({
-		_id: req.params.id
-	})
-		.then(deletedSpecies => {
-			res.json({ message: "Deleted Species", deletedSpecies })
-		})
-		.catch(err => {
-			console.log('Failed to delete: ', err)
-		})
+    Saved.findOneAndDelete({
+        _id: req.params.id
+    })
+        .then(deletedSpecies => {
+            res.json({ message: "Deleted Species", deletedSpecies })
+        })
+        .catch(err => {
+            console.log('Failed to delete: ', err)
+        })
 })
 
 router.post('/Species', requireToken, (req, res, next) => {
